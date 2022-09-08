4e0b509daae3bcb9f9e70e5bc8e713da534d0c1c
Jaseela99 <jaseelachittur@gmail.com>
2022-09-08 10:07:31 +0530

    added columns

---
+++ src/components/Columns.js
@@ -0,0 +1,28 @@
+export const Columns=[
+    //mapping each column to specific data value in the mock data 
+    {//column name
+        Header:"Id",
+        //accessing this key from mock data
+        accessor:"id",
+    },
+    {
+        Header:"First Name",
+        accessor:"first_name",
+    },
+    {
+        Header:"Last Name",
+        accessor:"last_name",
+    },
+    {
+        Header:"Date Of Birth",
+        accessor:"date_of_birth",
+    },
+    {
+        Header:"Country",
+        accessor:"country",
+    },
+    {
+        Header:"Phone Number",
+        accessor:"phone_number",
+    },
+]
